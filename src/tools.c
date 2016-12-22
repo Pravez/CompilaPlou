c0d56f91909b45d97b0f1f5db2a677420f27e1d7
Louis Deguillaume <louis.deguillaume@gmail.com>
2016-12-22 15:52:07 +0100

    Some starting point

---
+++ src/tools.c
@@ -0,0 +1,48 @@
+//
+// Created by kwetzakwak on 22/12/16.
+//
+#include <stdio.h>
+#include "tools.h"
+int current_regi = 0;
+int current_lbli = 0;
+char current_reg[10] = "x0";
+char current_lbl[10] = "l0";
+
+void debug(char* s, enum color c){
+    switch(c){
+        case RED:
+            printf("\033[31m%s\033[0m\n",s);
+            break;
+        case BLUE:
+            printf("\033[34m%s\033[0m\n",s);
+            break;
+        case GREEN:
+            printf("\033[32m%s\033[0m\n",s);
+            break;
+    }
+}
+
+void debugi(char* s, int i, enum color c){
+    switch(c){
+        case RED:
+            printf("\033[31m%s: %d\033[0m\n",s, i);
+            break;
+        case BLUE:
+            printf("\033[34m%s: %d\033[0m\n",s, i);
+            break;
+        case GREEN:
+            printf("\033[32m%s: %d\033[0m\n",s, i);
+            break;
+    }
+}
+
+int increment_register(){
+    int tmp = ++current_reg;
+    sprintf(current_reg, "x%i", tmp);
+    return tmp;
+}
+int increment_label(){
+    int tmp = ++current_lbli;
+    sprintf(current_reg, "l%i", ++current_lbli);
+    return tmp;
+}
---
+++ src/tools.h
@@ -0,0 +1,24 @@
+#ifndef PROJET_COMPILATION_TOOLS_H
+#define PROJET_COMPILATION_TOOLS_H
+
+enum color{
+    RED,
+    BLUE,
+    GREEN
+};
+/**
+ * Affiche du texte dans le terminal avec la couleur donnée
+ * @param s String Message à afficher
+ * @param c color Couleur à utiliser
+ */
+void debug(char* s, enum color c);
+
+/**
+ * Affiche du texte dans le terminal avec la couleur donnée
+ * @param s String Message à afficher
+ * @param i int Entier supplémentaire à afficher à la fin du message
+ * @param c color Couleur à utiliser
+ */
+void debugi(char* s, int i, enum color c);
+
+#endif //PROJET_COMPILATION_TOOLS_H

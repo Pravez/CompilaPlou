19aa54ace6ced0be70e894b6374082facb04b2d6
pravez <paul.ravezb@gmail.com>
2016-12-26 15:49:35 +0100

    started work on expressions

--- src/expression.h
+++ src/expression.h
@@ -2,33 +2,28 @@
 #define _EXPRESSION_H
 
 #include "type.h"
-#include "tools.h"
 
-#define DEFAULT_SIZE 100
-#define DEFAULT_CONSTANT_SIZE 50
-#define DEFAULT_FUNCTION_SIZE 100
-
-enum LLVM_TYPE{
-    LLVM_I32, //Integer
-    LLVM_DOUBLE, //Float
-    LLVM_I8, //Character
-    LLVM_I32_PTR,
-    LLVM_DOUBLE_PTR,
-    LLVM_I8_PTR
+enum EXPR_COMMAND{
+    ADD, SUB, DIV, CONV_2DOUBLE, CONV_2INT, MUL, FMUL
 };
 
-struct llvm__program{
-    int validity;
-    int line_number;
-    int size;
-    char** code;
+enum OPERAND_TYPE{
+    VARIABLE, INT, DOUBLE
 };
 
-void llvm__init_program(struct llvm__program* program);
-int llvm__program_add_line(struct llvm__program* program, char* line);
-char* llvm__create_constant(char* name, enum TYPE type, int size, void* value);
-char* llvm___create_function_def(struct Function function);
-char* type_of(enum LLVM_TYPE type);
-enum LLVM_TYPE llvm__convert(enum TYPE type);
+struct Expression{
+    char* register_name;
+    enum TYPE type;
+    union expr_operand operands[2];
+};
+
+struct expr_operand{
+    enum OPERAND_TYPE type;
+    union operand{
+        char* variable;
+        int int_value;
+        double double_value;
+    };
+};
 
 #endif //_EXPRESSION_H
